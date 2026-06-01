30ba6ebc4c68cd258b8a38b4a125416f6ee26c1d
dhrutube <dhruvsehgal2554@gmail.com>
2026-05-31 19:19:04 -0700

    removed demand vs bus routes

--- app.py
+++ app.py
@@ -10,15 +10,11 @@ tab1, tab2, tab3, tab4 = st.tabs(["Pedestrian Heatmap", "Demand v.s. Bus Routes"
 with tab1:
     with open("html_visualizations/san_diego_heatmap.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
-
-with tab2:
-    with open("html_visualizations/demand_vs_bus_routes.html", "r") as f:
-        st.components.v1.html(f.read(), height=700, scrolling=False)
         
-with tab3:
+with tab2:
     with open("html_visualizations/underserved_zones_map_income.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
         
-with tab4:
+with tab3:
     with open("html_visualizations/underserved_zones_map_density.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
