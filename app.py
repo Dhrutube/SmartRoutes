b2c58644d6b0ad392a6723829a1321110ff800b1
Alice Park <67348917+starryyeyed@users.noreply.github.com>
2026-05-31 19:04:32 -0700

    final changes final final

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="SmartRoutes", layout="wide")
 st.title("SmartRoutes — San Diego MTS")
 st.markdown("🚌 A geospatial data project aimed at improving San Diego MTS bus routes by analyzing population density and pedestrian foot-traffic.")
 
-tab1, tab2, tab3 = st.tabs(["Pedestrian Heatmap", "Demand v.s. Bus Routes", "Map of Underserved Zones"])
+tab1, tab2, tab3, tab4 = st.tabs(["Pedestrian Heatmap", "Demand v.s. Bus Routes", "Map of Underserved Zones Based on Income", "Map of Underserved Zones Based on Density"])
 
 with tab1:
     with open("Outputs/san_diego_heatmap.html", "r") as f:
@@ -16,5 +16,9 @@ with tab2:
         st.components.v1.html(f.read(), height=700, scrolling=False)
         
 with tab3:
+    with open("html_visulizations/underserved_zones_map.html", "r") as f:
+        st.components.v1.html(f.read(), height=700, scrolling=False)
+        
+with tab4:
     with open("outputs_seena/underserved_zones_map.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
