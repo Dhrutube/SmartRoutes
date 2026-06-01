5a138bb2aa0659b94f46ca550e1965d0677aad1f
dhrutube <dhruvsehgal2554@gmail.com>
2026-05-31 19:08:15 -0700

    updated html file paths in app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ st.markdown("🚌 A geospatial data project aimed at improving San Diego MTS bus
 tab1, tab2, tab3, tab4 = st.tabs(["Pedestrian Heatmap", "Demand v.s. Bus Routes", "Map of Underserved Zones Based on Income", "Map of Underserved Zones Based on Density"])
 
 with tab1:
-    with open("Outputs/san_diego_heatmap.html", "r") as f:
+    with open("html_visualizations/san_diego_heatmap.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
 
 with tab2:
-    with open("outputs_seena/demand_vs_bus_routes.html", "r") as f:
+    with open("html_visualizations/demand_vs_bus_routes.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
         
 with tab3:
-    with open("html_visualizations/underserved_zones_map.html", "r") as f:
+    with open("html_visualizations/underserved_zones_map_density.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
         
 with tab4:
-    with open("outputs_seena/underserved_zones_map.html", "r") as f:
+    with open("html_visualizations/underserved_zones_map_income.html", "r") as f:
         st.components.v1.html(f.read(), height=700, scrolling=False)
